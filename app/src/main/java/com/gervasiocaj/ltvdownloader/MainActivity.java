d572bbfc0f004e1e4ee2785173df945b798dc55a
Gervasio Junior <juniorb08@gmail.com>
2016-10-22 19:57:44 -0300

    Commit inicial

---
+++ app/src/main/java/com/gervasiocaj/ltvdownloader/MainActivity.java
@@ -0,0 +1,10 @@
+package com.gervasiocaj.ltvdownloader;
+
+import android.app.Activity;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity {
+
+}
